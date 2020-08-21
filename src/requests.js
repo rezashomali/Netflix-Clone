82f49a29161c596143827ad7196291499b91c532
reza shomali nezhad <r.shomalinezhad@gmail.com>
2020-08-21 11:47:02 +0200

    adding api requsets with axios

---
+++ src/requests.js
@@ -0,0 +1,16 @@
+const API_KEY = "78b37d26bab7de6194c851cd8f104c7a";
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+
+export default requests;
+
+// https://api.themoviedb.org/3/discover/movie?api_key=78b37d26bab7de6194c851cd8f104c7a&with_genres=10749
